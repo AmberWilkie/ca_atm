39ffaf653f2e28faeae0d5cbbccd90d34c1389ad
Amber <amber@amberwilkie.com>
2016-08-08 17:26:57 +0200

    added tests for withdraw and refactored

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,29 @@
+require 'date'
+
 class Atm
   attr_accessor :funds
 
   def initialize
     @funds = 1000
   end
+
+  def withdraw(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account) then
+      return
+    else
+      perform_transaction(amount, account)
+    end
+  end
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance -= amount
+    expected_output = { status: true, message: 'success', date: Date.today, amount: amount }
+  end
+
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,27 @@
 require './lib/atm.rb'
 
 describe Atm do
+  let(:account) { double('Account') }
+
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
+
   it 'has $1k on initialize' do
     expect(subject.funds).to eq 1000
   end
+
+  it 'reduces funds at withdrawal' do
+    subject.withdraw(50, account)
+    expect(subject.funds).to eq 950
+  end
+
+  it 'allows withdrawal if account has the funds available' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
+
+
 end
