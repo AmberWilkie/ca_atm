72aa0cc6ab905b0fc73253d82714f17b98661361
Amber <amber@amberwilkie.com>
2016-08-09 21:17:52 +0200

    changed 'name' to 'attrs[:name] and tests 1 and 2 working now

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,13 @@ class Person
   attr_accessor :name, :cash, :account
   @name = nil
 
-  def initialize(args = {}) #it's not taking name: 'thomas' from the spec file and I don't know why. Works if I put name: 'thomas' in here.
-    @name = name
+  def initialize(attrs = {}) #it's not taking name: 'thomas' from the spec file and I don't know why. Works if I put name: 'thomas' in here.
+    @name = attrs[:name]
     access_user_name(name)
 
   end
 
-  def access_user_name(name)
+  def access_user_name(attrs = {})
     if name != nil
       @name = name
     else
